c026c81e88914f02db3a4e2763df99523dfbcb35
Eugene Novikov <eugene.work.4940@gmail.com>
2023-09-30 20:03:25 +0300

    Пофиксил подключение базы данных

--- src/our-team.js
+++ src/our-team.js
@@ -3,6 +3,10 @@ import { Error, Footer, Header } from './components';
 import { Favorites, Main, Member } from './pages';
 import { ERROR } from './constants';
 import styled from 'styled-components';
+import { useDispatch, useSelector } from "react-redux";
+import { selectMembers } from "./redux/selectors";
+import { useEffect } from "react";
+import { getMembersAsync } from "./redux/actions";
 
 const AppColumn = styled.div`
 	display: flex;
@@ -19,6 +23,15 @@ const Page = styled.div`
 `;
 
 export const OurTeam = () => {
+	const dispatch = useDispatch();
+	const members = useSelector(selectMembers);
+
+	useEffect(() => {
+		dispatch(getMembersAsync);
+	}, [dispatch]);
+
+	console.log('main: ', members);
+
 	return (
 		<AppColumn>
 			<Header />
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,19 +1,6 @@
 import styled from 'styled-components';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getMembersAsync } from '../../redux/actions';
-import { selectMembers } from '../../redux/selectors';
 
 const MainContainer = ({ className }) => {
-	const dispatch = useDispatch();
-	const members = useSelector(selectMembers);
-
-	useEffect(() => {
-		dispatch(getMembersAsync);
-	}, [dispatch]);
-
-	console.log('main: ', members);
-
 	return <></>;
 };
 
